94e847252dd0498cb4c0ab3749f225ca09d8e005
Michael Neidhardt <mneidhardt@gmail.com>
2016-05-07 11:20:24 +0200

    Added arg charset.

--- src/main/java/dk/meem/text/Application.java
+++ src/main/java/dk/meem/text/Application.java
@@ -1,35 +1,48 @@
 package dk.meem.text;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.text.Normalizer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class Application {
 
     public static void main( String[] args ) {
-    	int lowerbound = 127;	// All code points above this will be reported. 
+    	int lowerbound = 127;	// All code points above this will be reported.
+    	String charset = Charset.defaultCharset().name();
+
     	try {
     		if (args.length == 0) {
     			throw new Exception("Filename missing.");
     		}
     		if (args.length > 1) {
     			lowerbound = Integer.parseInt(args[1]);
+    			if (args.length > 2) {
+    				charset = args[2];
+    			}
     		}
-        	new Application().run(args[0], lowerbound);
+        	new Application().run(args[0], lowerbound, charset);
     	} catch (Exception e) {
-    		System.err.println("Args: filename [lowerbound]");
+    		System.err.println("Args: filename [lowerbound [encodingcharset]]");
     		System.err.println("lowerbound: All codepoints equal to or greater than this, will be reported. Default is 127");
+    		System.err.println("encodingcharset: E.g 'UTF8'. Default is ??");
+    		
     	}
     }
     
-    public void run(String filename, int lowerbound) {
-    	try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-
+    public void run(String filename, int lowerbound, String charset) throws FileNotFoundException, UnsupportedEncodingException {
+    	System.out.println("Using charset " + charset);
+    	//try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+        try {
+        	BufferedReader br = new BufferedReader(
+     			   new InputStreamReader(
+     	                      new FileInputStream(filename), charset));
+        	
 			String line;
 			int linecount=0;
 			
@@ -52,9 +65,9 @@ public class Application {
 				}
 			}
 
+			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
     }
-    
 }
